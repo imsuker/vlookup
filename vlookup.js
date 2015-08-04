a3c2964b935c81736fd52f0572b54b197923f263
gukw <38353285@qq.com>
2015-08-04 16:38:52 +0800

    add time log

--- vlookup.js
+++ vlookup.js
@@ -3,35 +3,41 @@ var xlsx = require('node-xlsx');
   var Dota = {
     config : {
       infoOfOriginExcel : { //源excel信息
-        nameExcel : "base.xlsx",
+        nameExcel : "test_base.xlsx",
         lineBase : 1,  //基于baseline行进行vlookup
         lineBeginInsert : 6,//从第5行开始写入
       },
       aInfoOfTargetExcels : [//目标excels信息
         {
-          nameExcel : "a.xlsx",
-          lineBase : 2, //基于lineBase比对
-          lineWatch : 5  //观察值
-        },
-        {
-          nameExcel : "b.xlsx",
-          lineBase : 3, //基于baseline比对
-          lineWatch : 4  //观察值
+          nameExcel : "test_target.xlsx",
+          lineBase : 3, //基于lineBase比对
+          lineWatch : 7  //观察值
         }
-
       ]
     },
     init : function(){
+      console.log('begin Time', new Date());
+      Dota.config.beginTime = new Date();
       Dota.run();
     },
+    log : function(text){
+      var dDate = new Date();
+      var nMinutes = dDate.getMinutes();
+      var sDate = [dDate.getHours(), nMinutes < 10?'0'+nMinutes : nMinutes, dDate.getSeconds()].join(':') + '.' + dDate.getMilliseconds();
+      console.log(sDate, text);
+    },
     run : function(){
+      Dota.log('begin loading origin excel');
       var aRowsBaseExcel = xlsx.parse(Dota.config.infoOfOriginExcel.nameExcel)[0].data; //baseExcel的数据
+      Dota.log('end loading origin excel');
+      Dota.log('begin loading target excels');
       //存放所有targetExcels的数据
       var aDataOfTargetExcels = [];
       Dota.config.aInfoOfTargetExcels.forEach(function(oInfoOfTargetExcel, i){
         var oData = xlsx.parse(oInfoOfTargetExcel.nameExcel)[0].data;
         aDataOfTargetExcels.push(oData);
       });
+      Dota.log('end loading target excels');
 
       //开始运算
       var lineBeginInsert = Dota.config.infoOfOriginExcel.lineBeginInsert;
@@ -46,18 +52,19 @@ var xlsx = require('node-xlsx');
         var valueOfCellBase = oRowBaseExcel[Dota.config.infoOfOriginExcel.lineBase-1]; 
         //遍历目标表找寻目标单元格
         Dota.config.aInfoOfTargetExcels.forEach(function(oInfoOfTargetExcel, i){//遍历目标表
-          console.log('rowOfBaseExcel', rowOfBaseExcel,'targetExcel',oInfoOfTargetExcel.nameExcel,'valueOfCellBase', valueOfCellBase);
+          //console.log('rowOfBaseExcel', rowOfBaseExcel,'targetExcel',oInfoOfTargetExcel.nameExcel,'valueOfCellBase', valueOfCellBase);
           var nLineWillInsert = lineBeginInsert -1 + i; //每行vlookup结果放置再该列
           var aAllDataOfTargetExcel = aDataOfTargetExcels[i] || [];//取出目标表的所有行
           var bMatch = false;
           aAllDataOfTargetExcel.some(function(aDataOfTargetRow, rowTarget){//遍历目标表的每行
             var valueOfCheckCellTargetLine = aDataOfTargetRow[oInfoOfTargetExcel.lineBase-1]; //目标表的目标列的值
-            console.log('rowTarget', rowTarget,'lineTarget', oInfoOfTargetExcel.lineBase-1,'valueOfLineTarget', valueOfCheckCellTargetLine);
+            //console.log('rowTarget', rowTarget,'lineTarget', oInfoOfTargetExcel.lineBase-1,'valueOfLineTarget', valueOfCheckCellTargetLine);
             if(valueOfCellBase == valueOfCheckCellTargetLine){//比较，如果相等，写入baseexcel的base行+base列
               var result = aDataOfTargetRow[oInfoOfTargetExcel.lineWatch-1];
               oRowBaseExcel[nLineWillInsert] = result;
               bMatch = true;
               console.log('match succsss!','nLineWillInsert', nLineWillInsert + 1,'lineWatch',oInfoOfTargetExcel.lineWatch-1 + 1, 'result', result);
+              return true;
             }
           });
           if(!bMatch){
@@ -68,6 +75,8 @@ var xlsx = require('node-xlsx');
       var buffer = xlsx.build([{name:"result", data : aRowsBaseExcel}]);
       var fs = require('fs');
       fs.writeFileSync('result.xlsx', buffer);
+      Dota.config.endTime = new Date();
+      console.log('执行时间：',Dota.config.endTime - Dota.config.beginTime, ' 毫秒');
     },
     fun : null
   };
