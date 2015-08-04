2a66066e3992c004cf449d9e5943639322095f48
gukw <38353285@qq.com>
2015-08-04 12:04:03 +0800

    逻辑测试OK

--- vlookup.js
+++ vlookup.js
@@ -2,21 +2,21 @@ var xlsx = require('node-xlsx');
 ~function(){
   var Dota = {
     config : {
-      chartBaseExcel : {
+      infoOfOriginExcel : { //源excel信息
         nameExcel : "base.xlsx",
-        baseLine : 1,  //基于baseline行进行vlookup
-        nMaxLine : 5,//从第5行开始写入
+        lineBase : 1,  //基于baseline行进行vlookup
+        lineBeginInsert : 6,//从第5行开始写入
       },
-      aTargetExcel : [
+      aInfoOfTargetExcels : [//目标excels信息
         {
           nameExcel : "a.xlsx",
-          baseLine : 2, //基于baseline比对
-          watchLine : 5  //观察值
+          lineBase : 2, //基于lineBase比对
+          lineWatch : 5  //观察值
         },
         {
           nameExcel : "b.xlsx",
-          baseLine : 3, //基于baseline比对
-          watchLine : 4  //观察值
+          lineBase : 3, //基于baseline比对
+          lineWatch : 4  //观察值
         }
 
       ]
@@ -25,37 +25,47 @@ var xlsx = require('node-xlsx');
       Dota.run();
     },
     run : function(){
-      var aChartBaseRow = xlsx.parse(Dota.config.chartBaseExcel.nameExcel)[0].data;
-      var aTargetData = [];
-      Dota.config.aTargetExcel.forEach(function(oTargetExcel, i){
-        var oData = xlsx.parse(oTargetExcel.nameExcel)[0].data;
-        aTargetData.push(oData);
+      var aRowsBaseExcel = xlsx.parse(Dota.config.infoOfOriginExcel.nameExcel)[0].data; //baseExcel的数据
+      //存放所有targetExcels的数据
+      var aDataOfTargetExcels = [];
+      Dota.config.aInfoOfTargetExcels.forEach(function(oInfoOfTargetExcel, i){
+        var oData = xlsx.parse(oInfoOfTargetExcel.nameExcel)[0].data;
+        aDataOfTargetExcels.push(oData);
       });
 
-      var nMaxLine = Dota.config.chartBaseExcel.nMaxLine;
-      aChartBaseRow.forEach(function(oRow, row){ //遍历基本表的每行
-        if(row == 0){
-          Dota.config.aTargetExcel.forEach(function(oTarget, i){
-            oRow[nMaxLine-1 + i] = oTarget.nameExcel;
+      //开始运算
+      var lineBeginInsert = Dota.config.infoOfOriginExcel.lineBeginInsert;
+      aRowsBaseExcel.forEach(function(oRowBaseExcel, rowOfBaseExcel){ //遍历基本表的每行
+        if(rowOfBaseExcel == 0){//第一列是输出标题，不用运算
+          Dota.config.aInfoOfTargetExcels.forEach(function(oTarget, i){
+            oRowBaseExcel[lineBeginInsert-1 + i] = oTarget.nameExcel;
           });
           return;
         }
-        var baseCell = oRow[Dota.config.chartBaseExcel.baseLine-1]; 
-        Dota.config.aTargetExcel.forEach(function(oTargetExcel, i){//遍历目标表
-          console.log('row', row,'i',i);
-          var nTargetLine = nMaxLine -1 + i;
-          var oTargetRow = aTargetData[i][row] || [];
-          oTargetRow.some(function(aTargetRow, targetRow){//遍历目标表的每行
-            var targetCell = aTargetRow[oTargetExcel.baseLine-1];
-            console.log('target', oTargetExcel.nameExcel, 'row', row,'line', oTargetExcel.baseLine-1,'targetCell', targetCell);
-            if(baseCell == targetCell){
-              oRow[nTargetLine] = aTargetRow[oTargetExcel.watchLine-1];
-              return true;
-            } 
+        //将要比较的值
+        var valueOfCellBase = oRowBaseExcel[Dota.config.infoOfOriginExcel.lineBase-1]; 
+        //遍历目标表找寻目标单元格
+        Dota.config.aInfoOfTargetExcels.forEach(function(oInfoOfTargetExcel, i){//遍历目标表
+          console.log('rowOfBaseExcel', rowOfBaseExcel,'targetExcel',oInfoOfTargetExcel.nameExcel,'valueOfCellBase', valueOfCellBase);
+          var nLineWillInsert = lineBeginInsert -1 + i; //每行vlookup结果放置再该列
+          var aAllDataOfTargetExcel = aDataOfTargetExcels[i] || [];//取出目标表的所有行
+          var bMatch = false;
+          aAllDataOfTargetExcel.some(function(aDataOfTargetRow, rowTarget){//遍历目标表的每行
+            var valueOfCheckCellTargetLine = aDataOfTargetRow[oInfoOfTargetExcel.lineBase-1]; //目标表的目标列的值
+            console.log('rowTarget', rowTarget,'lineTarget', oInfoOfTargetExcel.lineBase-1,'valueOfLineTarget', valueOfCheckCellTargetLine);
+            if(valueOfCellBase == valueOfCheckCellTargetLine){//比较，如果相等，写入baseexcel的base行+base列
+              var result = aDataOfTargetRow[oInfoOfTargetExcel.lineWatch-1];
+              oRowBaseExcel[nLineWillInsert] = result;
+              bMatch = true;
+              console.log('match succsss!','nLineWillInsert', nLineWillInsert + 1,'lineWatch',oInfoOfTargetExcel.lineWatch-1 + 1, 'result', result);
+            }
           });
+          if(!bMatch){
+              oRowBaseExcel[nLineWillInsert] = '#N/A';
+          }
         });
       });
-      var buffer = xlsx.build([{name:"result", data : aChartBaseRow}]);
+      var buffer = xlsx.build([{name:"result", data : aRowsBaseExcel}]);
       var fs = require('fs');
       fs.writeFileSync('result.xlsx', buffer);
     },
